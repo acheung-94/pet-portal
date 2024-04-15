7a0db785fa19a143acbe41ec4996af7ef6502d08
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-04-15 09:51:15 -0700

    add COIRS

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 
+const cors = require('cors')
+const {isProduction} = require('./config/keys')
 require('dotenv').config()
 
 var app = express()
@@ -12,6 +14,11 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
+
+if (!isProduction) {
+	app.use(cors())
+}
+
 
 require('./app/routes/router').default(app)
 
