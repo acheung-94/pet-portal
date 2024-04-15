212f064ca87d14d404dd8c6c1313e005ff0614ab
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-04-15 09:51:06 -0700

    fix linter errors

--- backend/app/models/User.js
+++ backend/app/models/User.js
@@ -2,16 +2,16 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    hashedPassword: {
-        type: String,
-        required: true
-    }
+	username: {
+		type: String,
+		required: true
+	},
+	hashedPassword: {
+		type: String,
+		required: true
+	}
 } , {
-    timestamps: true
+	timestamps: true
 })
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema)
