9c6efa88bcd8ad4e237f648a8dbd861e3e0d350c
sam-kim99 <samkim0451@gmail.com>
2024-04-16 16:36:14 -0400

    add petReducer and petApiUtils files

---
+++ frontend/src/utils/petApiUtils.js
@@ -0,0 +1,33 @@
+import jwtFetch from "./jwt";
+
+export const getPets = () => (
+    jwtFetch(`/api/pets`, {
+        method: 'GET'
+    })
+)
+
+export const getPet = petId => (
+    jwtFetch(`/api/pets/${petId}`, {
+        method: 'GET'
+    })
+)
+
+export const postPet = petInfo => (
+    jwtFetch(`/api/pets`, {
+        method: 'POST',
+        body: JSON.stringify(petInfo)
+    })
+)
+
+export const putPet = petInfo => (
+    jwtFetch(`/api/pets/${petInfo.id}`, {
+        method: 'PUT',
+        body: JSON.stringify(petInfo) 
+    })
+)
+
+export const deletePet = petId => (
+    jwtFetch(`/api/pets/${petId}`, {
+        method: 'DELETE',
+    })
+)
