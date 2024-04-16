320eecb20393fcef6e72c47585689ea6f1540a28
Christopher Trent <149724464+ChristopherJTrent@users.noreply.github.com>
2024-04-16 12:37:31 -0700

    update pets router and adjust create and update methods (#11)
    Co-authored-by: sam-kim99 <samkim0451@gmail.com>

--- backend/app/routes/pets.js
+++ backend/app/routes/pets.js
@@ -1,12 +1,13 @@
-import PetController from '../controllers/PetController.js'
 import {Router} from 'express'
+import PetController from '../controllers/PetController.js'
+import { requireUser } from '../../config/passport.js'
 
 const petRouter = Router()
 
-petRouter.get('/', PetController.index)
-petRouter.get('/:id', PetController.show)
-petRouter.post('/', PetController.create)
-petRouter.put('/:id', PetController.update)
-petRouter.delete('/:id', PetController.delete)
+petRouter.get('/', requireUser, PetController.index)
+petRouter.get('/:id', requireUser, PetController.show)
+petRouter.post('/', requireUser, PetController.create)
+petRouter.put('/:id', requireUser, PetController.update)
+petRouter.delete('/:id', requireUser, PetController.delete)
 
 export default petRouter
