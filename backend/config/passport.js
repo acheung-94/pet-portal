ca97d8bcd2b40c595ffa4635f17cd8ffee82e1b9
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-04-16 10:57:23 -0700

    fix bug in passport

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,12 +1,11 @@
+import {default as User} from '../app/models/User.js'
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 import LocalStrategy from 'passport-local'
-import mongoose from 'mongoose'
 import passport from 'passport'
 import passportJwt from 'passport-jwt'
 const { Strategy: JwtStrategy, ExtractJwt } = passportJwt
 import {secretOrKey} from './keys.js'
-const User = mongoose.model('User')
-import jwt from 'jsonwebtoken'
 
 passport.use(new LocalStrategy({
 	session: false,
@@ -24,18 +23,18 @@ passport.use(new LocalStrategy({
 
 export const loginUser = async (user) => {
 	const userInfo = {
-	  _id: user._id,
-	  username: user.username,
-	  email: user.email
+		_id: user._id,
+		username: user.username,
+		email: user.email
 	}
 	const token = await jwt.sign(
-	  userInfo, // Payload
-	  secretOrKey, // Sign with secret key
-	  { expiresIn: 3600 } // Tell the key to expire in one hour
+		userInfo, // Payload
+		secretOrKey, // Sign with secret key
+		{ expiresIn: 3600 } // Tell the key to expire in one hour
 	)
 	return {
-	  user: userInfo,
-	  token
+		user: userInfo,
+		token
 	}
 }
 const options = {}
@@ -44,10 +43,10 @@ options.secretOrKey = secretOrKey
 
 passport.use(new JwtStrategy(options, async (jwtPayload, done) => {
 	try {
-	  const user = await User.findById(jwtPayload._id)
-	  if (user) {
+		const user = await User.findById(jwtPayload._id)
+		if (user) {
 		// return the user to the frontend
-		return done(null, user)
+			return done(null, user)
 	  }
 	  // return false since there is no user
 	  return done(null, false)
@@ -57,11 +56,11 @@ passport.use(new JwtStrategy(options, async (jwtPayload, done) => {
 	}
 }))
 
-export const requireUser = passport.authenticate('jwt', { session: false });
+export const requireUser = passport.authenticate('jwt', { session: false })
 
 export const restoreUser = (req, res, next) => {
 	return passport.authenticate('jwt', { session: false }, (err, user) => {
-	  if (user) req.user = user;
-	  next();
-	})(req, res, next);
-  };
+	  if (user) req.user = user
+	  next()
+	})(req, res, next)
+}
