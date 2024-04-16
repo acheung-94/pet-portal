011905495916660a111b4e76784936ce6dbcd562
sam-kim99 <samkim0451@gmail.com>
2024-04-16 13:59:58 -0400

    create PetGrid component

---
+++ frontend/src/components/PetGrid/PetGrid.jsx
@@ -0,0 +1,40 @@
+import { useSelector } from 'react-redux'
+import { selectCurrentUser } from '../../store/sessionReducer'
+import './PetGrid.css'
+
+const PetGrid = () => {
+    const currentUser = useSelector(selectCurrentUser)
+    const currentPets = currentUser.pets // placeholder
+
+    return (
+        <>
+            <div className='pet-grid-container'>
+                <div className='pet-grid-header-container'>
+                    <div className='pet-grid-header-text'>
+                        <h1>Your Pets</h1>
+                    </div>
+                    <div className='get-grid-header-form'>
+                        {/* add new pet form */}
+                    </div>
+                </div>
+                <div className='pet-grid-divider'>
+                    
+                </div>
+                <div className='pet-grid-index'>
+                    {currentPets.map(pet => (
+                        <div key={pet.id} className='pet-item'>
+                            <div className='pet-name'>
+                                <span>{pet.name}</span>
+                            </div>
+                            <div className='pet-img'>
+                                {/* pet's image */}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default PetGrid
