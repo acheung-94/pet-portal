0de81dd948c4f2fcdb44400be995222e09c32dbc
acheung-94 <acheung@ucdavis.edu>
2024-04-16 11:17:50 -0700

    adjust dashboard styling

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -4,19 +4,21 @@ import './Dashboard.css'
 const Dashboard = () => {
 
     return(
-        <>
+        <div className='page-container'>
             <Navbar />
             <div className='dashboard-container'>
                 <h1 className='dashboard-header'>Dashboard</h1>
+                <span className='dashboard-sep'></span>
                 <div className="reminder-container">
                     <h2>Upcoming Reminders</h2>
                     <p> Reminders go here! </p>
                 </div>
+                <span className='dashboard-sep'></span>
                 <div className='index-container'>
                     
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
