63360516c610b49f3b17a056f2e8bffefad3db3f
Christopher Trent <149724464+ChristopherJTrent@users.noreply.github.com>
2024-04-18 11:29:01 -0700

    add file upload to pet router (#33)

--- backend/app/routes/pets.js
+++ backend/app/routes/pets.js
@@ -1,12 +1,13 @@
 import PetController from '../controllers/PetController.js'
 import { requireUser } from '../../config/passport.js'
 import {Router} from 'express'
+import { singleMulterUpload } from '../../util/s3Utils.js'
 
 const petRouter = Router()
 
 petRouter.get('/', PetController.index)
 petRouter.get('/:id', PetController.show)
-petRouter.post('/', requireUser, PetController.create)
+petRouter.post('/', requireUser, singleMulterUpload('image'), PetController.create)
 petRouter.put('/:id', requireUser, PetController.update)
 petRouter.delete('/:id', requireUser, PetController.delete)
 
