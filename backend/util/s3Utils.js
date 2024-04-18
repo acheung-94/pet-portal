8b78f32450463d9dfa71ca63b63b6a7ead6cf38a
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-04-18 09:54:12 -0700

    appease linter

--- backend/util/s3Utils.js
+++ backend/util/s3Utils.js
@@ -1,4 +1,4 @@
-import {S3Client, GetObjectCommand} from '@aws-sdk/client-s3'
+import {GetObjectCommand, S3Client} from '@aws-sdk/client-s3'
 import {getSignedUrl} from '@aws-sdk/s3-request-presigner'
 import multer from 'multer'
 import path from 'path'
@@ -51,7 +51,7 @@ export const retrievePrivateFileUrl = async key => {
 
 export const storage = multer.memoryStorage({
 	destination: function (req, file, callback) {
-		callback(null, "")
+		callback(null, '')
 	}
 })
 
