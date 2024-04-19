9bb1f3b0c11754bfe302f6c4c92622adf05ae0b7
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-04-19 16:22:13 -0700

    remove index router and associated files

--- backend/app/routes/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-
-const indexRouter = Router()
-/* GET home page. */
-indexRouter.get('/', function(req, res, _) {
-	res.render('index', { title: 'Express' })
-})
-
-export default indexRouter
