cf567eca60b8c9334a7a6953f2695267e453fd9b
acheung-94 <152105091+acheung-94@users.noreply.github.com>
2024-04-18 23:42:33 -0700

    Bugfix/reminders (#40)
    * fix overflow on pet profile and remove footer
    
    * add styling to pet summary
    
    * wrap /dashboard to include navbar and outlet to prevent unmounting
    
    * resolve page overflow issues on dash and profile
    
    * add styling to footer links
    
    * reorganize dashboard, extend refresh threshold to 5 sec

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,17 @@ import Home from "./components/Home/Home"
 import Auth from "./components/Auth/Auth"
 import Dashboard from "./components/Dashboard/Dashboard";
 import PetProfile from "./components/PetProfile/PetProfile";
+import Navbar from "./components/Navbar/Navbar";
+import { Outlet } from "react-router-dom";
 const router = createBrowserRouter([
   {path: '/', element: <Home />},
   {path: '/login', element: <Auth />},
   {path: '/register', element: <Auth />},
-  {path: '/dashboard', children: [
+  {path: '/dashboard', element: 
+      <>
+        <Navbar />
+        <Outlet />
+      </>, children: [
     {index: true, element: <Dashboard />},
     {path: ':petId', element: <PetProfile />}
   ]}
