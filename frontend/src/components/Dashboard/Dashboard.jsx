40192b702d2c89ffd1e055ea4d1eb7c476a380db
acheung-94 <acheung@ucdavis.edu>
2024-04-18 23:40:58 -0700

    reorganize dashboard, extend refresh threshold to 5 sec

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -5,17 +5,23 @@ const Dashboard = () => {
 
     return(
         <div className='page-container'>
-            <div className='dashboard-container'>
                 <h1 className='dashboard-header'>Your Dashboard</h1>
                 <span className='dashboard-sep'></span>
-                <div className="reminder-container">
-                    <h2>Upcoming Reminders</h2>
-                    <p> Reminders go here! </p>
-                </div>
-                <span className='dashboard-sep'></span>
+            <div className='dashboard-container'>
+                
                 <div className='index-container'>
                     <PetGrid/>
                 </div>
+                <span className='dashboard-sep'></span>
+                <div className="right-container">
+                    <div className="all-reminders">
+                        <h3>Reminder container</h3>
+                    </div>
+                    <div className="find-vet-container">
+                        <h3>Find yoself a vet</h3>
+                    </div>
+                </div>
+                
             </div>
         </div>
     )
