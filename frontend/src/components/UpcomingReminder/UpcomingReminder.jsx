e9e5eac95438537af9f21059d6462d50f09d0475
shjang1025 <shjang1025@gmail.com>
2024-04-20 10:40:33 -0700

    put pet names, image on upcoming reminders

--- frontend/src/components/UpcomingReminder/UpcomingReminder.jsx
+++ frontend/src/components/UpcomingReminder/UpcomingReminder.jsx
@@ -1,9 +1,10 @@
 import './UpcomingReminder.css'
 import {useDispatch} from 'react-redux' 
-import {fetchUserReminders, selectReminder} from '../../store/reminderReducer'
+import {fetchUserReminders, selectReminders} from '../../store/reminderReducer'
+import { selectPets } from '../../store/petReducer'
 import { selectCurrentUser } from "../../store/sessionReducer"
 import { useSelector } from 'react-redux'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSyringe, faPills} from "@fortawesome/free-solid-svg-icons"
 import { faCalendarCheck} from "@fortawesome/free-regular-svg-icons"
@@ -11,41 +12,89 @@ import { faCalendarCheck} from "@fortawesome/free-regular-svg-icons"
 const UpcomingReminder = () => {
     const dispatch = useDispatch()
     const currentUser = useSelector(selectCurrentUser)
-    const reminders = useSelector(selectReminder) //all reminders [{}, {},..]
+    const reminders = useSelector(selectReminders) //all reminders [{}, {},..]
+    const pets = useSelector(selectPets)
     const currentUserReminder = reminders.filter(ele  => ele.user === currentUser._id.toString())
-    const [typeState, setTypeState] = useState('')
-
-    useEffect(() => {
-        dispatch(fetchUserReminders(currentUser._id))
-    }, [dispatch])
-
     const today = new Date()
+    const diffDays = []
+    const petIds = []
     const upcomingReminders = currentUserReminder.filter((ele,idx) => {
         let due = new Date(currentUserReminder[idx].dueDate)
         let diff = Math.ceil((due - today) / (1000 * 60 * 60 * 24));
         if(diff <= 5) {
+            diffDays.push(diff)
+            petIds.push(ele.pet)
             return ele
         }
     })
-
-    console.log(upcomingReminders)
-
+    console.log(petIds)
+    
+    const petNames = []
+    const petImages = []
+    // for(let i = 0; i< petIds.length; i++) {
+    //     for(let j = 0; j < pets.length; j++) {
+    //         if(petIds[i] && petIds[i] === pets[j]._id) {
+    //             petNames.push(pets[j].name)
+    //             break
+    //         } else if(petIds[i] === undefined){
+    //             petNames.push(undefined)
+    //             break
+    //         }
+    //     }
+    // }
+    for(let i = 0; i< petIds.length; i++) {
+        for(let j = 0; j < pets.length; j++) {
+            if(petIds[i] && petIds[i] === pets[j]._id) {
+                petNames.push(pets[j].name)
+                petImages.push(pets[j].imageUrl)
+                break
+            } else if(petIds[i] === undefined){
+                petNames.push(undefined)
+                petImages.push(undefined)
+                break
+            }
+        }
+    }
+    console.log(petImages)
+    useEffect(() => {
+        dispatch(fetchUserReminders(currentUser._id))
+    }, [dispatch])
+    
     return(
         <>
             {upcomingReminders.map((ele, idx) => (
 
                 <div key={idx} className='upcoming-reminder-container'>
-                    <div className={`upcoming-reminder-${typeState}-icon`}>
-                        {ele.type === 'appointment' && <FontAwesomeIcon className="appointment-icon" icon={faCalendarCheck} style={{fill: "black"}}size="xl"/>}
-                        {ele.type === 'vaccination' && <FontAwesomeIcon className="vaccination-icon" icon={faSyringe} size="xl"/>}
-                        {ele.type === 'medication' && <FontAwesomeIcon className="medication-icon" icon={faPills} size="xl" />}       
+                    <div className='upcoming-reminder-icon-container'>
+                        <div className={`upcoming-reminder--icon`}>
+                            {ele.type === 'appointment' && <FontAwesomeIcon className="appointment-icon" icon={faCalendarCheck} size="large"/>}
+                            {ele.type === 'vaccination' && <FontAwesomeIcon className="vaccination-icon" icon={faSyringe} size="large"/>}
+                            {ele.type === 'medication' && <FontAwesomeIcon className="medication-icon" icon={faPills} size="large" />}       
+                        </div>
+                        <div className='upcoming-reminder-type'>
+                            {ele.type === 'appointment' && 'appointment'}
+                            {ele.type === 'vaccination' && 'vaccination'}
+                            {ele.type === 'medication' && 'medication'}       
+                        </div>
                     </div>
  
                     <div className='upcoming-reminder-content'>
                         <div>{ele.title}</div>
+                        <div>
+                            {petNames[idx] && <p>{petNames[idx]}</p>}
+                        </div>
+                        <div className='upcoming-pet-image'>
+                            {petImages[idx] && <img id='upcoming-pet-image' src={petImages[idx]}/>}
+                        </div>
                     </div>
                     <div className='upcoming-reminder-pet'>
-                        <div></div>
+                        <div className='upcoming-days'>
+                            <p>in {diffDays[idx]} days</p>
+                        </div>
+                        <div className='upcoming-reminder-petnames'>
+                            
+                            
+                        </div>
                     </div>
                 </div>
             ))}
