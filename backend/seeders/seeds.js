1377b0a2f2a3affab574db2167c659b30faca3ed
sam-kim99 <samkim0451@gmail.com>
2024-04-20 20:59:32 -0400

    create seeds file

---
+++ backend/seeders/seeds.js
@@ -0,0 +1,109 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const { faker } = require('@faker-js/faker')
+const { mongoURI: db } = require('../config/keys.js')
+const User = require('../models/User')
+const Pet = require('../models/Pet')
+const Reminder = require('../models/Reminder')
+
+const NUM_SEED_USERS = 10
+const NUM_SEED_PETS = 10
+const NUM_SEED_REMINDERS = 30
+
+mongoose
+	.connect(db, { useNewUrlParser: true })
+	.then(() => {
+		insertSeeds()
+	})
+	.catch(err => {
+		console.error(err.stack)
+		process.exit(1)
+	})
+
+
+const users = []
+for (let i = 1; i < NUM_SEED_USERS; i++) {
+	const firstName = faker.name.firstName()
+	const lastName = faker.name.lastName()
+	users.push(
+		new User ({
+			email: faker.internet.email(firstName, lastName),
+			hashedPassword: bcrypt.hashSync(faker.internet.password(), 10)
+		})
+	)
+}
+
+const pets = []
+for (let i = 0; i < NUM_SEED_PETS; i++) {
+	const pet = new Pet({
+		name: faker.name.firstName(),
+		dob: faker.date.past(10),
+		sex: faker.helpers.randomize(['male', 'female', 'unknown']),
+		species: faker.helpers.randomize(['dog', 'cat', 'bird']),
+		color: faker.commerce.color(),
+		breed: faker.animal.dog(),
+		microchipNumber: faker.datatype.number(),
+		insurancePolicyId: faker.finance.account(),
+		weight: faker.datatype.float({ min: 5, max: 50 }),
+		owner: users[i % NUM_SEED_USERS]._id,
+		imageUrl: faker.image.animals()
+	})
+	pets.push(pet)
+}
+
+const reminders = []
+for (let i = 0; i < NUM_SEED_REMINDERS; i++) {
+	const type = faker.helpers.randomize(['appointment', 'vaccination', 'medication'])
+	let titleOptions = []
+
+	if (type === 'vaccination') {
+		titleOptions = ['Rabies', 'FVRCP', 'FELV']
+	} else if (type === 'appointment') {
+		titleOptions = ['Wellness', 'Illness', 'Procedure']
+	} else if (type === 'medication') {
+		titleOptions = ['Flea/Tick Prevention', 'Heartworm Prevention']
+	}
+
+	const reminder = new Reminder({
+		title: faker.helpers.randomize(titleOptions),
+		dueDate: faker.date.future(1),
+		performDate: faker.date.future(2),
+		description: faker.lorem.sentence(),
+		location: faker.address.cityName(),
+		type: type,
+		pet: pets[i % NUM_SEED_PETS]._id,
+		user: users[i % NUM_SEED_USERS]._id
+	})
+
+	reminders.push(reminder)
+}
+
+const insertSeeds = () => {
+	console.log('Resetting db and seeding users, pets, and reminders...')
+  
+	// Reset collections
+	User.collection.drop()
+		.then(() => Pet.collection.drop())
+		.then(() => Reminder.collection.drop())
+		.then(() => {
+		
+			console.log('Inserting users...')
+			return User.insertMany(users)
+		})
+		.then(() => {
+			console.log('Inserting pets...')
+			return Pet.insertMany(pets)
+		})
+		.then(() => {
+			console.log('Inserting reminders...')
+			return Reminder.insertMany(reminders)
+		})
+		.then(() => {
+			console.log('Done!')
+			mongoose.disconnect()
+		})
+		.catch(err => {
+			console.error('Error during seeding:', err.stack)
+			process.exit(1)
+		})
+}
