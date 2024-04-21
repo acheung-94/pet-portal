8cd53f6f9c7bd33e88e197fb4e7efa9cf6462ee1
acheung-94 <acheung@ucdavis.edu>
2024-04-21 12:05:19 -0700

    enhance styling of home page

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -35,21 +35,27 @@ const Home = () => {
                             <span className="deco-3"></span>
                         </div>
                         <div className="welcome-message">
-                            <p> Whether you&apos;re a seasoned pet parent or embarking on your journey
+                            <div className="welcome-message-1">
+                                <span className='deco-1'></span>
+                                <p>Whether you&apos;re a seasoned pet parent or embarking on your journey
                                  with a new furry friend, Pet Portal is here to simplify your pet care 
-                                 routine. From managing reminders and appointments to tracking your 
-                                 pet&apos;s health metrics, our platform is designed to make pet ownership 
-                                 a breeze. Join us now to ensure you never miss a beat in your pet&apos;s
-                                  care routine. With Pet Portal, staying on top of your pet&apos;s 
-                                  health and happiness has never been easier! Welcome aboard! </p>
-                            <div className="h2-splash"> <h3> ✨New here? Get started below. </h3> </div>
-                            <div className="welcome-links">
-                                <div className="demo-login" onClick={handleDemoLogin}>
-                                    <span className='deco-1'></span>
-                                    <span className='deco-2'></span>
-                                     Log in as a demo user
+                                 routine.</p>
+                            </div>
+                            <div className="welcome-message-2">
+                                <span className='deco-1'></span>
+                                <p>Keep track of your pet&apos;s important information, health reminders, and appointments all in one convenient location!</p>
+                            </div>
+                            <div className="welcome-message-3">
+                                <span className='deco-1'></span>
+                                <div className="welcome-links">
+                                    <div className="h2-splash"> <h3> ✨ Ready to get started? </h3> </div>
+                                    <div className="demo-login" onClick={handleDemoLogin}>
+                                        <span className='deco-2'></span>
+                                        Log in as a demo user
+                                    </div>
                                 </div>
                             </div>
+                            
                         </div>
                     </div>
                 </div>
