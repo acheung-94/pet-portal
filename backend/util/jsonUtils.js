c3f9c7ee8d5d83084f190caaa8fe9277e19b024b
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-04-29 12:30:21 -0700

    Document convertObjectToStateShape

--- backend/util/jsonUtils.js
+++ backend/util/jsonUtils.js
@@ -1,4 +1,9 @@
 
+/**
+ * Maps an array of objects to an object that associates an element with its id
+ * @param {{_id: string}[]} object An array of objects that have an _id prop
+ * @returns {Object}
+ */
 export function convertObjectToStateShape(object) {
 	return {
 		...Object.values(object)
