459f8df6a431e461a2b8a645d3be4e97a2a2f0c5
acheung-94 <acheung@ucdavis.edu>
2024-04-30 12:45:14 -0700

    rebase and run build

--- frontend/src/components/NewPetFormModal/NewPetFormModal.jsx
+++ frontend/src/components/NewPetFormModal/NewPetFormModal.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import './NewPetFormModal.css'
 import { useDispatch } from 'react-redux'
 import { createPet, updatePet } from '../../store/petReducer'
